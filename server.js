c135d661b523356990d6be99573c8ddbcf738da0
olof <15236616162@163.com>
2018-10-19 19:12:51 +0800

    test

--- server.js
+++ server.js
@@ -1,36 +1,11 @@
-var http = require('http');
-const https = require('https');
+var net = require('net');
+var server = new net.createServer();
 
-const options = {
-  hostname: 'api.douban.com',
-  port: 443,
-  path: '/v2/movie/top250',
-  method: 'GET'
-};
-var str = '';
-var arr = [];
-const req = https.request(options, (res) => {
-  res.on('data', (d) => {
-    str+=d
-  });
-  res.on('end',()=>{
-    var list=JSON.parse(str).subjects
-    list.map(function(item){
-        arr.push(item.title)
+var obj = new Object();
+var i = 0;
+
+server.on('connection',()=>{
+    user.on('data',(data)=>{
+        console.log('客户端：'+data);
     })
-
-  })
-});
-http.createServer((req,res)=>{
-	res.writeHead(200,{'content-type':'text/html;charset=utf-8','Access-Control-Allow-Origin':'*'})
-	if(req.url!='/favicon.ico'){
-		res.write(JSON.stringify(arr))
-		res.end()
-	}
-}).listen(3000)
-
-
-req.on('error', (e) => {
-  console.error(e);
-});
-req.end();
+})
